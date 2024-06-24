5bcd41cc6ef2881cec076b32bdb75c09ecdc65c5
sijingl2019 <sijingl2013@outlook.com>
2024-06-24 17:01:43 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="container"></div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+  import { onMounted } from 'vue';
+  import { Graph } from '@antv/g6';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  onMounted(() => {
+    const graph = new Graph({
+      container: document.getElementById('container'),
+      width: 500,
+      height: 500,
+      data: {
+        nodes: [
+          {
+            id: 'node-1',
+            style: { x: 50, y: 100 },
+          },
+          {
+            id: 'node-2',
+            style: { x: 150, y: 100 },
+          },
+        ],
+        edges: [{ id: 'edge-1', source: 'node-1', target: 'node-2' }],
+      },
+    });
+
+    graph.render();
+  });
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
